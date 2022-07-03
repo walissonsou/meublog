b639e8e927949f41fbf3e2c9260fb36e49eeccdd
walissonsou <walissonsouza96@hotmail.com>
2022-07-03 15:37:48 -0300

    consumo api

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,34 +1,30 @@
-import React, {useState } from 'react';
+import React, {useState, useEffect } from 'react';
 import axios from 'axios';
-
-import styles from './Project.module.css'
-
 
 export const Project = () => {
 
   const [ projects, setProjects ] = useState([])
- 
-  const getRepos = () => {
+  
+  useEffect(() => {
   axios.get('https://api.github.com/users/walissonsou/)repos')
     .then(res => res.json())
-    .then(data =>setProjects(data))
-    .catch(error => {
-      console.log(error);
-    })
-  };
+    .then(data => setProjects(data))
+ }, [])
+ 
+return (
+  <div>
+    <ul>
+      {
+        projects.map(projects => {
+          return (
+            <li>
+              <h3> {projects.name}</h3>
+            </li>
+          )
+        })
+      }
+    </ul>
+  </div>
 
-  return (
-    <div className={styles.container}>
-    <h2> Projetos no GitHub </h2>
-    <div>
-    {      
-      projects.map((project) =>
-      <div key={project.id}>
-      <p>{project.name}</p>      
-      </div>    
-     )
-  }
-    
-  </div>
-  </div>
-  )}
+)
+    }
