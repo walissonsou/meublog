8cb173fb31e3cae64e9cca9b3d3765f5d09e94d4
walissonsou <walissonsouza96@hotmail.com>
2022-11-21 11:19:54 -0300

    Preparando para deploy

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Header } from "../../Header/Header";
-import { Project } from "../../Projects";
+import { Header } from '../Header/Header'
+import { Project } from "../../components/Projects/index";
 
 export default function Home() {
   return (
