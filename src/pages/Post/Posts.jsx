30beefc4d913e80a45fcece5b6d8ec703a18a22a
walissonsou <walissonsouza96@hotmail.com>
2022-12-07 10:56:36 -0300

    build: function search of the posts

--- src/pages/Post/Posts.jsx
+++ src/pages/Post/Posts.jsx
@@ -7,26 +7,42 @@ export default function Posts() {
   const navigate = useNavigate();
 
   const [posts, setPosts] = useState([]);
+  const [ search, setSearch ] = useState('')
+  const [ filtredPosts, setFiltredPosts ] = useState([])
 
   useEffect(() => {
     getPosts().catch((err) => console.log(err));
   }, []);
 
-
   const getPosts = async () => {
     await apiposts.get('/')
       .then((res)  => setPosts(res.data));
   };
+
+  useEffect(() => {
+    setFiltredPosts(posts.filter(post => post.title.includes(search)))
+  }, [search])
   
   
   return (
     <>
       <header>
         <title> Posts | My Blog</title>
+       
       </header>
       <main className={styles.container}>
-        <div className={styles.posts}>
-          {posts.map((post) => {
+      <input           
+         name="search"
+         type="text"   
+         placeholder="Buscar post..."
+         onChange={e => setSearch(e.target.value).uppercase()}
+         value={search}         
+         
+         ></input>
+
+        { search.length > 0 ? (
+          <div className={styles.posts}>
+          {filtredPosts.map((post) => {
             const handleNavigateToPost = () => {
               navigate(`${post.id}`, {
                 state: { id: post.id, title: post.title, body: post.body },
@@ -48,6 +64,30 @@ export default function Posts() {
             );
           })}
         </div>
+        ) : 
+          <div className={styles.posts}>
+          {posts.map((post) => {
+            const handleNavigateToPost = () => {
+              navigate(`${post.id}`, {
+                state: { id: post.id, title: post.title, body: post.body },
+              });
+            };
+
+            return (
+              <div key={post.id} className={styles.postsu}>
+                <div>
+                  <time> </time>
+                  <h2>{post.title} </h2>
+                  <p>{post.body}</p>
+                </div>
+                <button> <a 
+                  onClick={() => handleNavigateToPost()} >
+                  Ler mais
+                </a>    </button>          
+              </div>
+            );
+          })}
+        </div>}
       </main>
     </>
   );
