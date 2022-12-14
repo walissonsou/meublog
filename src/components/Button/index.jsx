cea89c544b7fce41ed04702effd3be3269be9da7
walissonsou <walissonsouza96@hotmail.com>
2022-12-13 22:15:16 -0300

    build: create component Button

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./Button.module.css";
+
+export default function Button (props){
+  return (
+  <button type="button" className={styles.buttonabout}>
+    {props.name}
+  </button>
+  )
+}
