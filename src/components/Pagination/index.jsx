5333b51c7fe84fbe0d4faec45c43dfa2eb899f34
walissonsou <walissonsouza96@hotmail.com>
2022-12-20 14:00:25 -0300

    fix: style

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -4,7 +4,7 @@ export default function Pagination({pages, setCurrentPage, currentPage}) {
     <div className={styles.containerButton}> 
       {Array.from(Array(pages), (item,index  ) => {
         return  <button
-          style={index === currentPage ? {border: "1px solid blue" , background:"gray"}: null}
+          style={index === currentPage ? {border: "1px solid var(--blue-400)" , background:"gray"}: null}
          className={styles.button} value={index} onClick={(e) => setCurrentPage(Number(e.target.value))} > {index + 1} </button> })}  
       </div>
   )
